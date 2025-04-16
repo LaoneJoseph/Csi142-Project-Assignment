45c76625f79caaa327de043d647e1dfc1a5a4f29
PEARL-BW <itspearljohn105@gmail.com>
2025-04-16 11:09:55 +0200

    Add files via upload

---
+++ BuyPayDelivery.java
@@ -0,0 +1,45 @@
+public class BuyPayDelivery{
+
+    private String paymentMethod;
+    private double deliveryCosts;
+    private String deliveryPlace;
+    private String payerName;
+    private String payerEmail;
+    private String cardHolder;
+    private int cardNumber;
+
+
+    public BuyPayDelivery(String paymentMethod, double deliveryCosts, String deliveryPlace, 
+                           String payerName, String payerEmail, String cardHolder, int cardNumber){
+
+        this.paymentMethod = paymentMethod;
+        this.deliveryCosts = deliveryCosts;
+        this.deliveryPlace = deliveryPlace;
+        this.payerName = payerName;
+        this.payerEmail = payerEmail;
+        this.cardHolder = cardHolder;
+        this.cardNumber = cardNumber;
+    }
+
+    public String getPaymentMethod(){return paymentMethod;}
+    public double getDeliveryCosts(){return deliveryCosts;}
+    public String getDeliveryPlace(){return deliveryPlace;}
+    public String getPayerName(){return payerName;}
+    public String getPayerEmail(){return payerEmail;}
+    public String getCardHolder(){return cardHolder;}
+    public int getCardNumber(){return cardHolder;}
+
+    public void setPaymentMethod(String paymentMethod){this.paymentMethod = paymentMethod;}
+    public void setDeliveryCosts(int deliveryCosts){this.deliveryCosts = deliveryCosts;}
+    public void setDeliveryPlace(String deliveryPlace){this.deliveryPlace = deliveryPlace;}
+    public void setPayerName(String payerName){this.payerName = payerName;}
+    public void setPayerEmail(Sting payerEmail){this.payerEmail = payerEmail;}
+    public void setCardHolder(String cardHolder){this.cardHolder = cardHolder;}
+    public void setCardNumber(int cardNumber){this.cardNumber = cardNumber;}
+
+    @Override
+         public String toString(){
+            return paymentMethod + " ,"+"\n "+ deliveryCosts +",\n"+ deliveryPlace+",\n"+ payerName + ",\n"+payerEmail+ ",\n"+ cardHolder+",\n"+cardNumber+",\n";
+         }
+
+}
