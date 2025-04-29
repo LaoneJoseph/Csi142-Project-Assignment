455e123978e4480030693b7335a9bc9ef2cb8a1c
Pearl John <itspearljohn105@gmail.com>
2025-04-29 04:35:45 +0200

    Update InvalidProductException.java

--- InvalidProductException.java
+++ InvalidProductException.java
@@ -1,3 +1,14 @@
+/**Authors; Laone Joseph 202302410
+            Pearl John 202304426
+            Princess Maboshe 202304582
+            Kabo Moseki 202301582
+            Katlego Pansiri 202405134
+   file name; InvalidProductExeception.java
+   To compile; InvalidProductException.java
+   To execute; InvalidProductException
+   Description; For handle errors for invalid product name input functionality
+    */
+
 public class InvalidProductException extends Exception {
     public InvalidProductException(String message) {
         super(message);
