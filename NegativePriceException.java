4b8f5a13bff9b75662c2c6294e218decf4c81b99
Pearl John <itspearljohn105@gmail.com>
2025-04-29 04:37:29 +0200

    Update NegativePriceException.java

--- NegativePriceException.java
+++ NegativePriceException.java
@@ -1,3 +1,14 @@
+/**Authors; Laone Joseph 202302410
+            Pearl John 202304426
+            Princess Maboshe 202304582
+            Kabo Moseki 202301582
+            Katlego Pansiri 202405134
+   file name; NegativePriceException.java
+   To compile; javac NegativePriceException.java
+   To execute; java NegativeException
+   Description; For handle errors for hegative price values input functionality
+    */
+
 public class NegativePriceException extends Exception {
     public NegativePriceException() {
         super();
