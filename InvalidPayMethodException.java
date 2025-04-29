d5d8a5f3e6a6048e9a91a90e7c70d51505a2fd45
Pearl John <itspearljohn105@gmail.com>
2025-04-29 04:34:07 +0200

    Update InvalidPayMethodException.java

--- InvalidPayMethodException.java
+++ InvalidPayMethodException.java
@@ -1,3 +1,15 @@
+/**Authors; Laone Joseph 202302410
+            Pearl John 202304426
+            Princess Maboshe 202304582
+            Kabo Moseki 202301582
+            Katlego Pansiri 202405134
+   file name; InvalidPayMethodExeception.java
+   To compile; InvalidPayMethodException.java
+   To execute; InvalidPayMethodException
+   Description; For future enhancements for invalid payment method functionality
+    */
+
+
 public class InvalidPayMethodException extends Exception {
     public InvalidPayMethodException(String message) {
         super(message);
